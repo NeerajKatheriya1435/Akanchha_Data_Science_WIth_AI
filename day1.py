a1c5ce7b5c7f4fa5765a35d8a90055163d45526f
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-25 10:07:06 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,14 @@
+# print("Hello world")
+# Addition perfomed
+a=9
+b=6
+print(a+b)
+# Diffrence between
+"""
+dsgdfh
+dfgdfb
+fghfhb
+fghftrhtf
+"""
+
+print("Hello world")
---
+++ day2.py
@@ -0,0 +1,50 @@
+# print("Hello  world")
+# print('Hello  wo"rld')
+# print("Hello  wo'rld")
+# print("Hello \" world")
+# print("Hello \\nworld")
+# print("Hello\t\t\t\tworld")
+
+# akankshaKumari=67
+# akankshaKumari786586_y7uhui_knvkj=67
+# print(akankshaKumari786586_y7uhui_knvkj)
+
+# age=78
+# age=45
+# AGE=90
+# print(age)
+# print(AGE)
+
+# var1=45
+# var2=45.78
+# var3="Aman"
+# var4=True
+
+# print(var1)
+# print(type(var1))
+
+# print(var2)
+# print(type(var2))
+
+# print(var3)
+# print(type(var3))
+
+# print(var4)
+# print(type(var4))
+
+a=9
+b=7
+print(a+b)
+
+# number1=5
+
+# square=number1*number1
+# cube=number1*number1*number1
+
+# print(cube)
+
+length=6
+breadth=9
+area=length*breadth
+
+print(area)
