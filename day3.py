f8181f325968e1058495a5633ac1994bc0611820
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-25 11:05:47 +0530

    Operators and print function

---
+++ day3.py
@@ -0,0 +1,107 @@
+
+# this is my comment and it will not execute
+"""
+fghjkl
+gfdgd
+"""
+# var1574678354
+
+
+# print("Hello")
+
+# var1=89.567
+# print(var1)
+# print(type(var1))
+
+# var1=24
+# var2=7
+
+# var3=int(var1/var2)
+
+# print(var3)
+# print(type(var3))
+
+# f = 7.9
+# print(int(f))
+
+# str1="Hello hii"
+# str2="My name is Shubham"
+# print(str1,str2,"Manulally")
+
+name1="Shubham"
+name2="John"
+name3="Peter"
+
+# print(name1,name2,name3)
+
+# print(name1,end=" ")
+# print(name2)
+
+# print(name1,end=" ")
+# print(name2,end=" ")
+# print(name3)
+
+# print(name1,name2,name3,sep="-")
+
+# print(name1,name2,sep=" and ",end="-")
+# print(name3)
+# a="8"
+# b="7"
+# print(a+b)
+
+# print(name1+name2)
+
+# a=int(input("Enter Number 1: "))
+# b=int(input("Enter Number 2: "))
+# print(a+b)
+
+# name1=input("Enter your name: ")
+# print("Good Morning",name1)
+
+# Operators in python
+
+# Arithmetic Operators
+# a=3
+# b=9
+# print(a+b)
+# print(a-b)
+# print(a/b)
+# print(a*b)
+
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+# Comparison Operators
+
+# a=7
+# b=8
+
+# print(a>b)
+# print(a>=b)
+# print(a<b)
+# print(a<=b)
+# print(a==b)
+# print(a!=b)
+
+# Assigment Operators
+
+# number=6
+# number1=number1+5
+
+# number+=5
+# number-=3
+# number*=2
+# number/=4
+# number**=3
+
+# print(number)
+
+# Logical Operators
+
+# print(5<7 and 8>4 and 3==4)
+# print(5<7 or 8>4 or 3==4)
+
+# print(5<7 and 8>4 and 3==3)
+# print(5>7 or 8>14 or 3==7)
+print(not(8>6))
