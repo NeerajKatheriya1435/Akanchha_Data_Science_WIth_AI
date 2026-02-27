98faddd5208d9a9a5e76cf6698b2c6c5c231d321
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-27 10:54:14 +0530

    conditional statements and loops

---
+++ day5.py
@@ -0,0 +1,73 @@
+# print("hello")
+# a=8
+# b=5
+# print(a+b)
+
+# age=34
+
+# if(age>18):
+#     print("You can drive the car")
+#     if(age==45):
+#         print("You are in special age")
+#     else:
+#         print("You are no in special age")
+# else:
+#     print("You can not drive the car")
+
+# marks=96
+
+# if(marks>90):
+#     print("Excellent")
+# elif(marks>75):
+#     print("Grade A")
+# elif(marks>60):
+#     print("Grade B")
+# elif(marks>35):
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+# Match Case Statements
+
+# value=int(input("Enter a number 1 to 3: "))
+
+# match(value):
+#     case 1:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case _ if value==4:
+#         print("It is special Number")
+#     case _ :
+#         print("This is my default case")
+
+# value=int(input("Enter a number 1 to 3: "))
+
+# match(value):
+#     case 1|5|8|9:
+#         print("Sunday")
+#     case 2:
+#         print("Monday")
+#     case 3:
+#         print("Tuesday")
+#     case _ if value==4:
+#         print("It is special Number")
+#     case _ :
+#         print("This is my default case")
+
+# for i in range(3,21,4):
+#     print(i)
+# table=568
+
+# for i in range(1,11):
+#     print(table*i)
+
+# for i in range(11):
+#     print(i*2)
+
+# str1="Aknchha"
+
+# for i in str1:
+#     print(i)
