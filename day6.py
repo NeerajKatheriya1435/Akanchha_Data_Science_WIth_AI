e2858324b4f87d874b0ddc2f4d30f819e7dfa9b5
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-02-28 10:47:38 +0530

    function loops and break continue

---
+++ day6.py
@@ -0,0 +1,72 @@
+
+# for i in range(5,11,2):
+#     print(i)
+
+# str1="Shivam"
+
+# for i in str1:
+#     print(i)
+
+# for i in range(11):
+#     if(i==5):
+#         break
+#     print(i)
+
+# for i in range(11):
+#     if(i==5 or i==8):
+#         continue
+#     print(i)
+
+# for i in range(21):
+#     if(i%2==0):
+#         continue
+#     print(i)
+
+# def AddTwoNumber(num1,num2): # formal parameters
+#     a=num1
+#     b=num2
+#     print(a+b)
+
+
+# AddTwoNumber(5,8) # actual parameters
+# print("hello")
+# AddTwoNumber(3,4)
+
+# print("hello user")
+
+# def newFunc(num1,num2):
+    # print("Hello User")
+    # print("Ans sum is:",a+b)
+    # print("Bbyyye Bbyyye")
+#     a=num1
+#     b=num2
+#     c=num1+num2
+#     return c
+
+# # newFunc(4,8)
+# print(newFunc(6,2))
+
+# def greet():
+#     print("Hello Good Morning Mam")
+
+# def addTwoNum(num1,num2):
+#     print(num1+num2)
+
+# def mulTwoNum(num1,num2):
+#     return (num1*num2)
+
+# print(greet())
+# addTwoNum(6,9)
+# print(mulTwoNum(3,9))
+
+# def greet(name1="Saloni"):
+#     print("Hello Good Morning Mam",name1)
+
+# greet("Sakshi")
+# greet("Rupa")
+# greet()
+
+# Take two input from user and one opeartor crate calculator using match case
+# Take two input from user length and brteadth print area of rectangle using function and permeter
+# N number sum program for loop
+# Gretest among three numbers
