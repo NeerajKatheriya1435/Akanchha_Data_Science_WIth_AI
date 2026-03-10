1f48ff3bc33451446f5e453cf41c727aadeab5ae
Neeraj Katheriya <neerajkatheriya1435@gmail.com>
2026-03-10 14:49:20 +0530

    tuples strings and sets

---
+++ day7.py
@@ -0,0 +1,64 @@
+# Tuples data types
+
+# tup1=(45,67,34,54,32)
+
+tup1=(12,7,2021,34,23,34)
+
+# tup1[4]=788
+# tup1.append(56)
+
+# t1=list(tup1)
+# t1.append(56)
+
+# tup1=tuple(t1)
+
+# print(tup1)
+# print(tup1[1])
+# print(tup1.count(34))
+# print(tup1.index(34))
+# print(tup1[2:5])
+
+# name="Neeraj"
+# age=45
+
+# str1="My name is {1} and age is {0}"
+# print(str1.format(name,age))
+
+# print(str1.format(age,name))
+
+# print(f"My name is {name} and age is: {age}")
+# print("My name is %s and age is %d"%(name,age))
+
+# set1={3,4,45,56,67,77,67,3,4}
+# set1=set()
+# print(set1)
+# print(type(set1))
+# set1={3,4,5,6,7}
+
+# set1.add(4)
+
+# set1.remove(14)
+
+# set1.discard(14)
+# set1.pop()
+
+# set1.clear()
+# set1.update([5,7,90])
+
+# print(set1)
+
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+# print(set1.symmetric_difference(set2))
+
+# my_set={1,2,3,4,5}
+
+# if 22 in my_set:
+#     print("2 is present")
+
+# if 13 not in my_set:
+#     print("13 is not present")   
